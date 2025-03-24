f0cc23576ccb8d165df7b7ee9daae9e67a1d9522
laserwl <loobywm@gmail.com>
2025-03-24 12:04:22 -0400

    done

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
+# W. Looby
+# random average generator 
+# March 24 2025
+# This program creates 100 random numbers and then calculates the average
 
+import random
+
+numbers = []
+
+for _ in range(100):
+    numbers.append(random.randint(0, 100))
+
+print (numbers)
+
+total_sum = sum(numbers)
+count = len(numbers)
+
+average = total_sum / count
+
+print("the average is" ,average,)
